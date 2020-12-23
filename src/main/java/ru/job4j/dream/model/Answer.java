e117d33f89584ee8e924036afe64cf59a741515a
dmitriyermoshin19 <dmstrannik@yandex.ru>
2020-12-23 10:08:36 +0300

    4.1. JSON формат.[238554#378251]

---
+++ src/main/java/ru/job4j/dream/model/Answer.java
@@ -0,0 +1,17 @@
+package ru.job4j.dream.model;
+
+public class Answer {
+    private String text;
+
+    public Answer(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
